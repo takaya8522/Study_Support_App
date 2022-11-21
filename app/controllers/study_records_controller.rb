4d94d7df5be001c263d94d267b85c3f91729b31a
takaya suzuki <suzuki8522@yahoo.co.jp>
2022-11-21 23:52:08 +0900

    🔧study_recordsコントローラー変更

--- app/controllers/study_records_controller.rb
+++ app/controllers/study_records_controller.rb
@@ -1,13 +1,41 @@
 class StudyRecordsController < ApplicationController
+  before_action :set_blog, only: [:show, :edit, :update, :destroy]
+
   def index
+    @study_records = StudyRecord.all
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(task_params)
+    if @task.save
+      redirect_to tasks_path, notice: t('.study_record_created')
+    else
+      render :new
+    end
+  end
+
+  def show
   end
 
   def edit
   end
 
-  def new
+  def update
   end
 
-  def show
+  def destroy
+  end
+
+  private
+  def set_study_record
+    @study_record = StudyRecord.find(params[:id])
+  end
+
+  def study_record_params
+    params.require(:study_record).permit(:title, :content, :learning_cycle, :comprehension)
   end
 end
--- db/migrate/20221121135632_create_study_records.rb
+++ db/migrate/20221121135632_create_study_records.rb
@@ -2,7 +2,7 @@ class CreateStudyRecords < ActiveRecord::Migration[6.1]
   def change
     create_table :study_records do |t|
       t.string :title, null: false
-      t.text :description
+      t.text :content
       t.string :learning_cycle, null: false
       t.boolean :comprehension, null: false, default: false
 
