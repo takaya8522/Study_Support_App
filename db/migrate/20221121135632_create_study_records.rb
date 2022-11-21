235b3270b50dfb534d79a9f52b0a44619778f774
takaya suzuki <suzuki8522@yahoo.co.jp>
2022-11-21 23:35:00 +0900

    🔧migrationファイル変更

--- db/migrate/20221121135632_create_study_records.rb
+++ db/migrate/20221121135632_create_study_records.rb
@@ -1,10 +1,10 @@
 class CreateStudyRecords < ActiveRecord::Migration[6.1]
   def change
     create_table :study_records do |t|
-      t.string :title
+      t.string :title, null: false
       t.text :description
-      t.string :learning_cycle
-      t.boolean :comprehension
+      t.string :learning_cycle, null: false
+      t.boolean :comprehension, null: false, default: false
 
       t.timestamps
     end
