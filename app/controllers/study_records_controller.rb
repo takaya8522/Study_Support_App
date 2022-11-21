8a99762f94846a9dab65efa67883f6416c27f013
takaya suzuki <suzuki8522@yahoo.co.jp>
2022-11-21 22:58:44 +0900

    🍰study_recordsコントローラー作成

---
+++ app/controllers/study_records_controller.rb
@@ -0,0 +1,13 @@
+class StudyRecordsController < ApplicationController
+  def index
+  end
+
+  def edit
+  end
+
+  def new
+  end
+
+  def show
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  get 'study_records/index'
+  get 'study_records/edit'
+  get 'study_records/new'
+  get 'study_records/show'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
