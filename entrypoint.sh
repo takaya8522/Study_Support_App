8cdb3c437956a0b3306a3d3ad292337a34c6a872
takaya suzuki <suzuki8522@yahoo.co.jp>
2022-11-26 00:27:39 +0900

    🐛AWSデプロイのためのバグ修正

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Rails に対応したファイル server.pid が存在しているかもしれないので削除する。
-rm -f /myapplearning_management_app/tmp/pids/server.pid
+rm -f /learning_management_app/tmp/pids/server.pid
 
 # コンテナーのプロセスを実行する。（Dockerfile 内の CMD に設定されているもの。）
 exec "$@"
