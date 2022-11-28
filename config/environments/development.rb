eb9524f142d6326b810590d8a6f2f01cbca03b32
takaya suzuki <suzuki8522@yahoo.co.jp>
2022-11-28 13:25:02 +0900

    🔧devise:install実行/設定変更

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -38,6 +38,9 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
+  # devise用にdefault url optionを設定
+  config.action_mailer.default_url_options = { host: 'localhost', port: 80 }
+
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
