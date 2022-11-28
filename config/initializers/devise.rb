fce940ffebdba07ce73840130fe9b758867c8fb0
takaya suzuki <suzuki8522@yahoo.co.jp>
2022-11-28 20:13:56 +0900

    ✨Lineログイン機能追加

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -272,6 +272,7 @@ Devise.setup do |config|
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
+  config.omniauth :line, ENV['LINE_KEY'], ENV['LINE_SECRET']
 
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
@@ -308,4 +309,5 @@ Devise.setup do |config|
   # When set to false, does not sign a user in automatically after their password is
   # changed. Defaults to true, so a user is signed in automatically after changing a password.
   # config.sign_in_after_change_password = true
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  devise_for :users
+  # devise lineログインのため
+  devise_for :users, controllers: {
+    omniauth_callbacks: "omniauth_callbacks"
+  }
   root 'study_records#index'
   resources :study_records, only: [:index, :create, :new, :edit, :show, :update, :destroy]
 end
