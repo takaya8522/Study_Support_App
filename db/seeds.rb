004429f6b60c914ace3290066b6080cdb34f9f1d
takaya suzuki <suzuki8522@yahoo.co.jp>
2022-12-03 22:51:34 +0900

    🎨category用view作成

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,9 +2,9 @@ User.create!(name: "admins", email: "adminadmino@piyopiyo.com", password: "12345
 User.create!(name: "normal", email: "normalnormal@piyopiyo.com", password: "123456", password_confirmation: "123456", admin: false, provider: nil, uid: nil)
 
 50.times do
-  StudyRecord.create!(title: "英語の勉強",content: "単語を覚える",study_cycle: "一週間",comprehension: "まだ理解していません", user_id: User.first.id)
+  StudyRecord.create!(title: "英文法の勉強",content: "英文法の本を読む",study_cycle: "一週間",comprehension: "まだ理解していません", user_id: User.first.id)
 end
 
 50.times do
-  StudyRecord.create!(title: "プログラミングの勉強",content: "コードを覚える",study_cycle: "一週間",comprehension: "まだ理解していません", user_id: User.second.id)
+  StudyRecord.create!(title: "Rubyの勉強",content: "コードを覚える",study_cycle: "一週間",comprehension: "まだ理解していません", user_id: User.second.id)
 end
