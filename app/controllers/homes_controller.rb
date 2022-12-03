f035f465c9ae111a2a5a52d48f05dfccb95830ce
takaya suzuki <suzuki8522@yahoo.co.jp>
2022-12-03 20:45:07 +0900

    ✨ransack実装完了

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,13 +1,21 @@
 class HomesController < ApplicationController
   skip_before_action :authenticate_user! 
+  before_action :set_q, only: [:top]
 
   def top
     if user_signed_in?
       @user = User.find(current_user.id)
       # ページネーション用設定（N1対策済み）
-      @study_records = current_user.study_records.includes(:user).order(created_at: :desc).page(params[:page])
+      @study_records = @q.result.includes(:user).order(created_at: :desc).page(params[:page])
     end
   end
 
   def about; end
+
+  private
+
+  # ransack用設定
+  def set_q
+    @q = current_user.study_records.ransack(params[:q])
+  end
 end
