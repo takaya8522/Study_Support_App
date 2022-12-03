f788f5f55f2cda45ba4777078c15fed2a0353bfe
takaya suzuki <suzuki8522@yahoo.co.jp>
2022-12-03 10:20:25 +0900

    🔧rails-admin install

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   root "homes#top"
   get "homes/about", to: "homes#about"
   get "homes/index", to: "homes#index"
