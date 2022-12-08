224f6cda3c7caa7bf058643fe5f2249a5c366a08
takaya suzuki <suzuki8522@yahoo.co.jp>
2022-12-08 21:29:34 +0900

    ✨アイコン画像設定機能実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   has_many :study_records, dependent: :destroy
   has_many :categories, dependent: :destroy
+  has_one_attached :image
   
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
