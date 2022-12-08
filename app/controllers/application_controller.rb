da42ee0afd2dcb1f05240bbe46cca03cd47fa0f1
takaya suzuki <suzuki8522@yahoo.co.jp>
2022-12-08 19:59:49 +0900

    ✨通知機能実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,6 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   protected
-
   def configure_permitted_parameters
     added_attrs = [ :email, :name, :password, :password_confirmation ]
     devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
