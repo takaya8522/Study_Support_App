ef6718a9253e4612bf0ccfebaade7b6466888e0d
takaya suzuki <suzuki8522@yahoo.co.jp>
2022-12-09 14:56:56 +0900

    🔧categoryコントローラー/view修正

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -10,22 +10,28 @@ class CategoriesController < ApplicationController
     @category = Category.new(category_params)
     @category.user_id = current_user.id
     if @category.save
-      redirect_to request.referer
+      flash[:warning] = 'カテゴリーの追加に成功しました'
     else
-      render :new
+      flash[:danger] = 'カテゴリーの追加に失敗しました'
     end
+    redirect_to request.referer
   end
 
   def update
     if @category.update(category_params)
-      redirect_to request.referer
+      flash[:warning] = 'カテゴリーの更新に成功しました'
     else
-      render :edit
+      flash[:danger] = 'カテゴリーの更新に失敗しました'
     end
+    redirect_to request.referer
   end
 
   def destroy
-    @category.destroy
+    if @category.destroy
+      flash[:warning] = 'カテゴリーの削除に成功しました'
+    else
+      flash[:danger] = 'カテゴリーの削除に失敗しました'
+    end
     redirect_to request.referer
   end
 
