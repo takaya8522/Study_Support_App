7b660869d9645acd768e94033c654bc501b3a893
takaya suzuki <suzuki8522@yahoo.co.jp>
2022-12-10 21:57:07 +0900

    ✨グラフで忘却率を表示する機能実装

--- app/helpers/study_records_helper.rb
+++ app/helpers/study_records_helper.rb
@@ -23,19 +23,35 @@ module StudyRecordsHelper
     end
   end
 
-  # 現在時刻が通知時間よりも後の場合にtrueを返すメソッド
-  # def check_timing_bool(study_timing, review_count, current_time)
-  #   if review_count.zero?
-  #     return true if study_timing.first_timing <= current_time
-  #   elsif review_count == 1
-  #     return true if study_timing.second_timing <= current_time
-  #   elsif review_count == 2
-  #     return true if study_timing.third_timing <= current_time
-  #   elsif review_count == 3
-  #     return true if study_timing.forth_timing <= current_time
-  #   else
-  #     false
-  #   end
-  #   false
-  # end
+  def set_forgetting_curve(study_record)
+    # グラフの開始時刻日をレビューカウントごとに算出する
+    if study_record.review_count.zero?
+      study_record_time = study_record.created_at
+    elsif study_record.review_count == 1
+      study_record_time = study_record.timing_result[0][:first_record]
+    elsif study_record.review_count == 2
+      study_record_time = study_record.timing_result[0][:second_record]
+    elsif study_record.review_count == 3
+      study_record_time = study_record.timing_result[0][:third_record]
+    else
+      return
+    end
+
+    forgetting_curve = []
+    # 投稿時刻の1分前を記憶率0%として挿入
+    forgetting_curve.push([study_record_time.ago(1.minute), 0])
+    # 投稿時刻を記憶率100%として挿入
+    forgetting_curve.push([study_record_time, 100])
+    # 投稿時刻から20分後、1時間後、9時間後、1日後、2日後、6日後、1ヶ月後の記憶率を挿入
+    forgetting_curve.push([study_record_time.since(20.minutes), 58.2])
+    forgetting_curve.push([study_record_time.since(1.hour), 44.2])
+    forgetting_curve.push([study_record_time.since(9.hours), 35.8])
+    forgetting_curve.push([study_record_time.since(1.day), 33.8])
+    forgetting_curve.push([study_record_time.since(2.days), 27.2])
+    forgetting_curve.push([study_record_time.since(6.days), 25.2])
+    forgetting_curve.push([study_record_time.since(1.month), 21.2])
+
+    # 送る値は日付だけにする
+    forgetting_curve.map { |k, v| [k.strftime('%m/%d %H:%M'), v] }
+  end
 end
