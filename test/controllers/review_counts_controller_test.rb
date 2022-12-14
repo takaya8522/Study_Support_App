7eaf5fe71396135bd4543997a09420e120210898
takaya suzuki <suzuki8522@yahoo.co.jp>
2022-12-14 23:08:45 +0900

    🔧rspec:install実行

--- test/controllers/review_counts_controller_test.rb
+++
@@ -1,7 +0,0 @@
-require "test_helper"
-
-class ReviewCountsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
-end
--- test/models/timing_result_test.rb
+++
@@ -1,7 +0,0 @@
-require "test_helper"
-
-class TimingResultTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
