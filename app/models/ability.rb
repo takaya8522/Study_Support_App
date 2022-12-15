4d6892fafcaed26eadda2860d7387600b724305a
takaya suzuki <suzuki8522@yahoo.co.jp>
2022-12-16 04:52:19 +0900

    🐛cancancan設定バグ修正

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -5,12 +5,12 @@ class Ability
 
   def initialize(user)
 
-  # ログインしているかつadminカラムがtrueの場合
-  if user && user.admin?
-    # 全ての機能を使えるように設定
-    can :manage, :all
-    can :access_admin_users_page, :all
-  else
+    # ログインしているかつadminカラムがtrueの場合
+    if user && user.admin?
+      # 全ての機能を使えるように設定
+      can :manage, :all
+      can :access_admin_users_page, :all
+    end
 
     # Define abilities for the user here. For example:
     #
