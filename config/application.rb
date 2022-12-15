6700cd5afd7b46a14a27bcbe4558fe5d4bd10bf9
takaya suzuki <suzuki8522@yahoo.co.jp>
2022-12-15 13:31:42 +0900

    🔧action_mailer不要なため削除

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,14 @@
 require_relative "boot"
 
-require "rails/all"
+# action_mailerとtest_unitは不要なため削除
+# require "rails/all"
+require 'active_record/railtie'
+require 'action_controller/railtie'
+require 'action_view/railtie'
+require 'active_job/railtie'
+require 'action_cable/engine'
+require 'active_storage/engine'
+require 'sprockets/railtie'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
