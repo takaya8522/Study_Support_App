5044e8eaf2829be6954f960fff9dc8e4012632cc
takaya suzuki <suzuki8522@yahoo.co.jp>
2022-12-25 11:23:51 +0900

    🔧appkication_helper修正

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,5 @@
 module ApplicationHelper
-    # 各ページのタイトル設定
-  # Googleのような検索エンジンの検索結果一覧ページで活用されます
+  # 各ページのタイトル設定(検索エンジン用)
   def full_title(page_title = '')
     base_title = "StudySupport"
     if page_title.empty?
@@ -10,8 +9,7 @@ module ApplicationHelper
     end
   end
   
-  # 各ページの説明文 120文字前後
-  # Googleのような検索エンジンの検索結果一覧ページで活用されます
+  # 各ページの説明文 120文字前後(検索エンジン用)
   def full_description(page_description = '')
     base_description = "StudySupportは、学習に特化したタスク管理アプリです。忘却曲線に基いた学習スケジュールを自動的に作成し、あなたの学習をサポートします。"
     if page_description.empty?
@@ -21,8 +19,7 @@ module ApplicationHelper
     end
   end
   
-  # 各ページの説明文 50文字前後
-  # SNS等でシェアされた際に活用されます
+  # 各ページの説明文(SNS用)
   def og_description(page_description = '')
     base_description = "StudySupportは、学習に特化したタスク管理アプリです。"
     if page_description.empty?
@@ -32,8 +29,7 @@ module ApplicationHelper
     end
   end
   
-  # 各ページのイメージ画像
-  # SNS等でシェアされた際に活用されます
+  # 各ページのイメージ画像(SNS用)
   def og_image(page_image = '')
     base_image = "https://picsum.photos/id/20/1200/630/?blur=1"
     if page_image.empty?
