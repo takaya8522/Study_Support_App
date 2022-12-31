924b98b9d6338d987cdb8eec3e60c216c9110d93
takaya suzuki <suzuki8522@yahoo.co.jp>
2022-12-31 19:56:35 +0900

    🔧create_timing_dateメソッド作成

--- app/controllers/study_records_controller.rb
+++ app/controllers/study_records_controller.rb
@@ -25,21 +25,10 @@ class StudyRecordsController < ApplicationController
 
   def create
     @study_record = StudyRecord.new(study_record_params)
-    @study_record.user_id = current_user.id
 
     if @study_record.save
-      # 予定復習タイミングを同時に作成
-      if @study_record.study_cycle == 'default'
-        StudyTiming.create!(study_record_id: @study_record.id, first_timing: 1.day.from_now, second_timing: 2.days.from_now,
-                            third_timing: 6.days.from_now, fourth_timing:  1.month.from_now)
-      elsif @study_record.study_cycle == 'one_week'
-        StudyTiming.create!(study_record_id: @study_record.id, first_timing: 7.day.from_now, second_timing: 14.days.from_now,
-                            third_timing: 21.days.from_now, fourth_timing:  28.day.from_now)
-      else
-        StudyTiming.create!(study_record_id: @study_record.id, first_timing: 14.day.from_now, second_timing: 28.days.from_now,
-                            third_timing: 42.days.from_now, fourth_timing:  56.day.from_now)
-      end
-      TimingResult.create!(study_record_id: @study_record.id)
+      # 選んだstudy_cycleごとに、復習タイミングと空の学習結果データを作成
+      create_timing_date(@study_record.study_cycle)
       redirect_to root_path, notice: t('.study_record_created')
     else
       render :new
@@ -48,7 +37,17 @@ class StudyRecordsController < ApplicationController
 
   def update
     if @study_record.update(study_record_params)
-      redirect_to root_path, notice: t('.study_record_updated')
+      # study_timingとtiming_resultを一度削除する。
+      @study_record.study_timing.destroy_all
+      @study_record.timing_result.destroy_all
+
+      # 選んだstudy_cycleごとに、復習タイミングと空の学習結果データを作成
+      create_timing_date(@study_record.study_cycle)
+
+      # review_countをリセットする
+      @study_record.update(review_count: 0)
+
+      redirect_to user_study_record_path(user_id: current_user.id, id: @study_record.id ), notice: t('.study_record_updated')
     else
       render :edit
     end
@@ -72,5 +71,21 @@ class StudyRecordsController < ApplicationController
   # ransack用設定
   def set_q
     @q = StudyRecord.ransack(params[:q])
+  end
+
+  # 選んだstudy_cycleごとに、復習タイミングと空の学習結果データを作成
+  def create_timing_date(study_cycle)
+    case study_cycle
+    when 'default'
+      StudyTiming.create!(study_record_id: @study_record.id, first_timing: 1.day.from_now, second_timing: 2.days.from_now,
+                          third_timing: 6.days.from_now, fourth_timing:  1.month.from_now)
+    when 'one_week'
+      StudyTiming.create!(study_record_id: @study_record.id, first_timing: 7.day.from_now, second_timing: 14.days.from_now,
+                          third_timing: 21.days.from_now, fourth_timing:  28.day.from_now)
+    when 'two_weeks'
+      StudyTiming.create!(study_record_id: @study_record.id, first_timing: 14.day.from_now, second_timing: 28.days.from_now,
+                          third_timing: 42.days.from_now, fourth_timing:  56.day.from_now)
+    end
+    TimingResult.create!(study_record_id: @study_record.id)
   end
 end
