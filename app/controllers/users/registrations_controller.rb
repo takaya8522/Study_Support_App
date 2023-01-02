c8bb8c45a74adfe178e2ea093addf02ce1e498d5
takaya suzuki <suzuki8522@yahoo.co.jp>
2023-01-02 17:23:44 +0900

    🔧i18n修正

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -3,9 +3,9 @@ class Users::RegistrationsController < Devise::RegistrationsController
 
   def ensure_normal_user
     if resource.email == 'guest@example.com'
-      redirect_to root_path, alert: t('users.flash.guest_failure')
+      redirect_to root_path, alert: 'ゲストユーザーは削除できません。'
     elsif resource.email == 'guest_admin@example.com'
-      redirect_to root_path, alert: t('users.flash.guest_admin_failure')
+      redirect_to root_path, alert: 'ゲスト管理者ユーザーは削除できません。'
     end
   end
 end
--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -2,12 +2,12 @@ class Users::SessionsController < Devise::SessionsController
   def guest_sign_in
     user = User.guest
     sign_in user
-    redirect_to root_path, notice: t('users.flash.login_success')
+    redirect_to root_path, notice: 'ゲストユーザーとしてログインしました。'
   end
 
   def guest_admin_sign_in
     user = User.guest_admin
     sign_in user
-    redirect_to root_path, notice: t('users.flash.login_admin_success')
+    redirect_to root_path, notice: 'ゲスト管理者ユーザーとしてログインしました。'
   end
 end
